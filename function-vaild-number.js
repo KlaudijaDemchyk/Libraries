8733915092b6e13faf96c171d200ad1e20a08116
KlaudijaDemchyk <119439337+KlaudijaDemchyk@users.noreply.github.com>
2023-03-19 18:11:57 +0100

    Delete function-vaild-number.js

--- function-vaild-number.js
+++
@@ -1,9 +0,0 @@
-//validate the textfield for numeric value only
-function validate(num,error) {
-    if (isNaN(num)) {
-        error.innerHTML = "Enter Numeric value only";
-        return false;
-    } else {
-        return true;
-    }
-}
