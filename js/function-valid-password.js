11df7c1414e1ea4c0050f954aac051d33fafb5bb
KlaudijaDemchyk <119439337+KlaudijaDemchyk@users.noreply.github.com>
2023-03-19 18:22:27 +0100

    Delete function-valid-password.js

--- js/function-valid-password.js
+++
@@ -1,9 +0,0 @@
-//JavaScript Retype Password Validation
-function matchpass(firstpassword, secondpassword) {
-    if (firstpassword == secondpassword) {
-        return true;
-    } else {
-        alert("password must be same!");
-        return false;
-    }
-}
